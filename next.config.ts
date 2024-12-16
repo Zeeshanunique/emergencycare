0f8d23723cc8f5a8226901419fc6bab92c800cf3
Zeeshanunique <zeeshanunique2619@email.com>
2024-12-16 10:03:05 +0530

    fixed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Add your Next.js configuration options here */
+  "rules": {
+  "@typescript-eslint/no-unused-vars": "off"
+}
 };
 
 export default nextConfig;
